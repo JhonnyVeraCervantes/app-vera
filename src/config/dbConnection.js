fd8b7e3edc57e7d2a236474ff25de7a4a5240725
JhonnyVeraCervantes <45070851+JhonnyVeraCervantes@users.noreply.github.com>
2020-07-11 20:34:18 -0500

    Update dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 module.exports = () => {
   return mysql.createConnection({
-    host: '',
-    user: '',
-    password: '',
-    database: ''
+    host: '13.66.39.224',
+    user: 'root',
+    password: 'Base12345',
+    database: 'Prueba_Spotify'
   });
 }
